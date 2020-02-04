f765c020aa8e4663c689431173c1652861bbf47b
parsa mpsh <parsa84sh1384@gmail.com>
2020-02-04 19:01:14 +0330

    sync with framework

--- init.php
+++ init.php
@@ -1,24 +1,8 @@
 <?php
 
-
-
-
-
-if( ! function_exists('app_path')){
-	function app_path($path=''){
-	    return APP_PATH . $path;
-	}
-}
-
-if( ! function_exists('libs')){
-	function libs($path=''){
-	    return app_path('/vendor/crystalphp/crystal/src/Crystal' . $path);
-	}
-}
-
 define('CRYSTAL_START_TIME' , microtime());
 
-require_once app_path('/vendor/autoload.php');
+require_once APP_PATH . '/vendor/autoload.php';
 
 if(\Crystal\Http\Response::handle_file_request()){
 	return FALSE;
