9133ecd07b4438811e835957230be0336f296892
parsa mpsh <parsa84sh1384@gmail.com>
2020-02-06 14:47:46 +0330

    sync with 1.2.3 , add Guard provider , add Auth middleware

---
+++ app/providers/Guards.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Providers;
+
+use Crystal\App\Provider;
+use Crystal\Utilities\Guard;
+
+class Guards extends Provider{
+	public function boot(){
+		// use class Guard to define guards
+	}
+}
---
+++ middlewares/Auth.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Middlewares;
+
+use Crystal\Middlewares\Middleware;
+use Crystal\Http\Request;
+
+class Auth extends Middleware
+{
+	public function handle(Request $r){
+
+		if( ! auth()->user()){
+			return redirect(url('/login'));
+		}
+
+		return false; // don't delete this line
+	}
+}
