d3598fe61e7cf50af4541391750e474592d9a879
parsa mpsh <parsa84sh1384@gmail.com>
2020-02-24 13:26:45 +0330

    sync with framework changes , add .env.example

--- app/config.php
+++ app/config.php
@@ -16,14 +16,20 @@ return [
 
     'database' => [
         'use_db' => false,
-        'db' => 'db1',
+        'db' => 'mysql',
         'connections' => [
-            'db1' => [
+            'mysql' => [
                 'driver' => 'mysql',
                 'name' => env('DB_NAME'),
                 'host' => env('DB_HOST'),
                 'username' => env('DB_USERNAME'),
                 'password' => env('DB_PASSWORD'),
+            ],
+
+            'sqlite' => [
+                // php sqlite extension is required
+                'driver' => 'sqlite',
+                'src' => env('DB_FILE'),
             ],
         ],
     ],
