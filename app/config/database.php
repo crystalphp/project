e2627894b42d0468231882838d60bdde58321be3
parsa mpsh <parsa84sh1384@gmail.com>
2020-03-03 22:05:49 +0330

    add sql server database config

--- app/config/database.php
+++ app/config/database.php
@@ -15,7 +15,20 @@ return [
         'sqlite' => [
             // php sqlite extension is required
             'driver' => 'sqlite',
-            'src' => env('DB_FILE'),
+            'src' => env('DB_FILE' , '~/exampledb.sqlite'),
+        ],
+
+        'sqlsrv' => [
+            'driver' => 'sqlsrv',
+            'server' => env('DB_HOST'),
+            'database' => env('DB_NAME'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+        ],
+
+        'other' => [
+            'driver' => 'other',
+            'pdo_connection_string' => 'example:connection;string',
         ],
     ],
 ];
